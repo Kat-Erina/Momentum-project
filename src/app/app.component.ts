d9e7a798773d794616c58f5d787c4a1267d89bc0
Kat-Erina <nalchevanidzekaterina@gmail.com>
2025-03-18 01:28:03 +0400

    wip: employy-modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { SharedService } from './core/services/shared.service';
+import { EmployeeModalComponent } from './components/employee-modal/employee-modal.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, EmployeeModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Momentum-project';
+  service=inject(SharedService);
+  employeeModalIsOpen=this.service.employeeModalIsOpen;
 }
